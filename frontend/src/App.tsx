bbf3cfff4c5335b4e47f347ff6cd444c930b645c
DouglasMonteles <silvadouglas328@gmail.com>
2020-06-16 14:35:53 -0300

    Iniciando a construção das páginas do projeto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  function handleButtonClick() {
-    setCount(count + 1);
-  }
 
   return (
     <div>
-      <Header title="Hello World" />
-
-      <h1>{ count }</h1>
-
-      <button onClick={handleButtonClick}>add</button>
+      <h1>Helo world</h1>
     </div>
   );
 }
