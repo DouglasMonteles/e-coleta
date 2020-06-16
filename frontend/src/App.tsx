e9fbb88ab2c82ea13410266bc3d0cbd42d0e1ee0
DouglasMonteles <silvadouglas328@gmail.com>
2020-06-16 14:32:58 -0300

    Estado e imutabilidade

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Header from './Header';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleButtonClick() {
+    setCount(count + 1);
+  }
+
   return (
     <div>
       <Header title="Hello World" />
 
-      <h1>Conteudo</h1>
+      <h1>{ count }</h1>
+
+      <button onClick={handleButtonClick}>add</button>
     </div>
   );
 }
