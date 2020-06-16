fb42c14325fd2be3f92bf858a220788e615abfe3
DouglasMonteles <silvadouglas328@gmail.com>
2020-06-16 17:40:11 -0300

    Configuração das rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 
-import Home from './pages/Home';
+import Routes from './router';
 
 function App() {
 
   return (
     <div>
-      <Home />
+      <Routes />
     </div>
   );
 }
